be28fbe828fd053bbe462597cfae4054315513ef
Mike <michaelsal1904@gmail.com>
2026-05-12 10:15:22 +0100

    add date, pay validation and writes data to json file

--- earnings-logger.py
+++ earnings-logger.py
@@ -1,19 +1,87 @@
 import datetime
-# track earnings of every day at my job
-
-# detail the month/day and day of the week
-
-# establish an extra - eg. double runs
-## USE A DICTIONARY TO STORE DATES: PAY
+import json
+import sys
 
 STANDARD_PAY = 60
+current_date = False
 
-# take input - what is the date?
-print("YYYY/MM/DD: ")
+# open and read from 'earnings.json'
+try:
+    with open("earnings.json", "r") as f:
+        data = json.load(f)
+except (FileNotFoundError, json.JSONDecodeError):
+    data = {}
 
-# make sure input matches required date format (YYYY/MM/DD)
-try: 
-    verified_date = datetime.date.fromisoformat(input())
-    print(verified_date)
-except ValueError as ve:
-    print("Input requires proper format (YYYY-MM-DD)")
+def verify_date(date):
+    """Provides verification to the date format
+    
+    :param date: str - the input date
+    :return: str - verified date"""
+    while True:
+        try: 
+            datetime.date.fromisoformat(date)
+            break
+        except ValueError:
+            print("Input requires proper format (YYYY-MM-DD)")
+            date = input()
+    return date
+
+def verify_pay(pay, date):
+    """Provides verification and amount to the 'pay' and writes it the key date
+    
+    :param pay: str - 'Y' or 'N' depending on the answer to previously asked question"""
+
+    if pay == 'Y':
+        data[date] = STANDARD_PAY
+    else:
+        print("How much was recieved?")
+        data[date] = int(input())
+        while data[date] < 0:
+            print("Input can't be smaller than 0.")
+            data[date] = int(input())
+
+def write_to_memory(data):
+    """Writes given data to 'earnings.json'
+    
+    :param data: dict - empty, or loaded data from earnings.json"""
+
+    json_str = json.dumps(data, indent=4)
+    with open("earnings.json", "w") as f:
+        f.write(json_str)
+
+
+# checks if file was run with '--summary'
+if sys.argv[1] == '--summary':
+    print(data.values())
+# keeps taking date and its pay for input into 'data'
+while True:
+
+    # takes date input
+    print("YYYY-MM-DD: ")
+    date = input()
+
+    # verifies correct date format
+    verify_date(date)
+
+    # if the date is current, signals just 1 loop
+    if date == str(datetime.date.today()):
+        current_date = True
+    
+    # takes pay input for said date
+    print("Did you recieve standard pay? [Y/N]")
+
+    # if input doesn't match required answer(s), loop begins
+    pay = input().upper()
+    while pay != 'Y' and pay != 'N':
+        print("Please provide valid response [Y/N]")
+        pay = input().upper()
+
+    # verifies pay amount
+    verify_pay(pay, date)
+
+    # the new key-value pair is written to 'earnings.json'
+    write_to_memory(data)
+
+    if current_date:
+        break
+
